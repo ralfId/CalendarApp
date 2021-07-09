29df4a9c57f12fc5b6ff6ae8e666ff74b5b119e9
rafaelId <ralf_raid@outlook.es>
2021-07-08 21:52:16 -0600

    create auth actions, auth reducer cases and add autreducer to rootreducer

---
+++ src/actions/auth.js
@@ -0,0 +1,63 @@
+import Swal from "sweetalert2";
+import { fetchWithoutToken, fetchWithToken } from "../helpers/customFetch"
+import { types } from "../types/types";
+
+export const startLogin = (email, password) => {
+
+    return async (dispatch) => {
+        const resp = await fetchWithoutToken('auth', { email, password }, 'POST');
+        const body = await resp.json();
+
+        if (body.ok) {
+
+            localStorage.setItem('token', body.token);
+            localStorage.setItem('token-init-Date', new Date().getTime());
+            dispatch(login({ token: body.token, uid: body.uid }))
+        } else {
+            Swal.fire('Error', body.msg, 'error');
+        }
+    }
+}
+
+export const startRegister = (name, email, password) => {
+    return async (dispatch) => {
+        const resp = await fetchWithoutToken('auth/register', { name, email, password }, 'POST');
+        const body = await resp.json();
+
+        if (body.ok) {
+
+            localStorage.setItem('token', body.token);
+            localStorage.setItem('token-init-Date', new Date().getTime());
+            dispatch(login({ token: body.token, uid: body.uid }))
+
+        } else {
+            Swal.fire('Error', body.msg, 'error');
+        }
+    }
+}
+
+export const startChecking = () => {
+
+    return async (dispatch) => {
+        const resp = await fetchWithToken('auth/renew');
+        const body = await resp.json();
+
+        if (body.ok) {
+
+            localStorage.setItem('token', body.token);
+            localStorage.setItem('token-init-Date', new Date().getTime());
+            dispatch(login({ token: body.token, uid: body.uid }))
+
+        } else {
+            Swal.fire('Error', body.msg, 'error');
+            dispatch(checkingFinish());
+        }
+    }
+}
+
+const checkingFinish = () => ({ type: types.authCheckingFinish })
+const login = (user) => ({
+    type: types.authLogin,
+    payload: user
+})
+
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,11 +1,12 @@
 import { combineReducers } from "redux";
 import { calendarReducer } from "./calendarReducer";
 import { uiReducer } from "./uiReducer";
+import { authReducer} from './authReducer'
 
 export  const rootReducer = combineReducers({
     ui: uiReducer,
-    calendar: calendarReducer
-    //TODO: auhtReducer
+    calendar: calendarReducer,
+    auth: authReducer
 
 })
 
